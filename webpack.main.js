8d7eeda854410030b9adb70e0385680eb506ef35
rr1980 <rener1980@googlemail.com>
2019-11-29 18:22:37 +0100

    refactor

--- webpack.main.js
+++ webpack.main.js
@@ -1,38 +1,48 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-    target: "electron-main",
-    mode: 'development',
-    node: {
-        __dirname: false
-    },
-    entry: {
-        "main": './src/main.ts',
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                enforce: 'pre',
-                loader: 'tslint-loader',
-                options: {
-                    typeCheck: true,
-                    emitErrors: true
-                }
-            },
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
+module.exports = (env, argv) => {
+    console.log("main:", argv.mode || 'develop');
+    const isProd = (argv.mode === 'production' );
+
+    return {
+        target: "electron-main",
+        mode: 'development',
+        watch: false,
+        node: {
+            __dirname: false
+        },
+        entry: {
+            "main": './src/main.ts',
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    enforce: 'pre',
+                    loader: 'tslint-loader',
+                    options: {
+                        typeCheck: true,
+                        emitErrors: true
+                    }
+                },
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.js', '.ts', '.tsx', '.jsx', '.json']
+        },
+        plugins: [
+            new CleanWebpackPlugin()
         ],
-    },
-    resolve: {
-        extensions: ['.js', '.ts', '.tsx', '.jsx', '.json']
-    },
-    devtool: 'source-map',
+        devtool: 'source-map'
+    }
 };
--- webpack.renderer.js
+++ webpack.renderer.js
@@ -1,38 +1,69 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    target: "electron-renderer",
-    mode: 'development',
-    node: {
-        __dirname: false
-    },
-    entry: {
-        "renderer": './src/renderer/renderer.ts'
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                enforce: 'pre',
-                loader: 'tslint-loader',
-                options: {
-                    typeCheck: true,
-                    emitErrors: true
+
+module.exports = (env, argv) => {
+    console.log("renderer:", argv.mode || 'develop');
+    const isProd = (argv.mode === 'production' );
+
+    return {
+        target: "electron-renderer",
+        mode: 'development',
+        watch: isProd ? false : true,
+        node: {
+            __dirname: false
+        },
+        entry: {
+            "renderer": './src/renderer/renderer.ts',
+            "site": './src/renderer/site.css'
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    enforce: 'pre',
+                    loader: 'tslint-loader',
+                    options: {
+                        typeCheck: true,
+                        emitErrors: true
+                    }
+                },
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                            options: {
+                                hmr: !isProd,
+                            },
+                        },
+                        'css-loader',
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.js', '.ts', '.tsx', '.jsx', '.json']
+        },
+        plugins: [
+            new MiniCssExtractPlugin(),
+            new HtmlWebpackPlugin(
+                {
+                    filename: 'index.html',
+                    template: 'src/renderer/index.html'
                 }
-            },
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
+            )
         ],
-    },
-    resolve: {
-        extensions: ['.js', '.ts', '.tsx', '.jsx', '.json']
-    },
-    devtool: 'source-map',
+         devtool: 'source-map'
+    }
 };
